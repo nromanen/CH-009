149acad28ba6a17d6ff2f780ab7b2ad3a81ab525
unknown <dkozatc@chclass13.training.local>
2013-04-01 17:49:07 +0300

    Main project

---
+++ main/dbConnector.js
@@ -0,0 +1,205 @@
+var dbConnector = dbConnector || {};
+var App = App || {};
+
+(function () {
+
+	var localDatabase = {};
+	var dbName = "productDB";
+	
+	localDatabase.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+	localDatabase.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange;
+	localDatabase.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction;
+
+	localDatabase.indexedDB.onerror = function (e) {
+		console.log( "Database error: " + e.target.errorCode );
+	};
+
+	dbConnector.openDatabase = function () {
+	
+		var openRequest = localDatabase.indexedDB.open( dbName, 1 );
+		
+		openRequest.onerror = function (e) {
+			console.log("Database error: " + e.target.errorCode);
+		};
+		
+		openRequest.onsuccess = function ( event ) {
+		
+			localDatabase.db = openRequest.result;
+			console.log("database is open");
+			App.Events.trigger( 'fetchProducts' );
+			
+		};	
+	}
+
+	dbConnector.createDatabase = function () {
+		
+		console.log('Deleting local database');
+		
+		var deleteDbRequest = localDatabase.indexedDB.deleteDatabase(dbName);
+		
+		deleteDbRequest.onsuccess = function ( event ) {
+		
+			console.log('Database deleted');
+			var openRequest = localDatabase.indexedDB.open(dbName,1);
+			
+			openRequest.onerror = function (e) {
+				console.log("Database error: " + e.target.errorCode);
+			}
+			
+			openRequest.onsuccess = function ( event ) {
+				console.log("Database created");
+				localDatabase.db = openRequest.result;
+			}
+			
+			openRequest.onupgradeneeded = function (evt) {  
+			
+				console.log('Creating object stores');
+				
+				var productStore = evt.currentTarget.result.createObjectStore
+					("products", {keyPath: "id", autoIncrement: true});
+				productStore.createIndex("matIndex", "product", { unique: false });        
+				productStore.createIndex("priceIndex", "productPrice", { unique: false });
+				
+			}
+		
+		}
+		
+		deleteDbRequest.onerror = function (e) {
+			console.log("Database error: " + e.target.errorCode);
+		}
+		
+	}
+		
+		
+	dbConnector.addProduct = function ( material, price ) {
+
+		try {
+
+			var transaction = localDatabase.db.transaction("products", "readwrite");
+			var store = transaction.objectStore("products");            
+			
+			console.log(store);
+		  
+			if (localDatabase != null && localDatabase.db != null) {
+			
+				var request = store.put ({
+					matIndex : material,
+					priceIndex : price,
+				});
+				
+				request.onsuccess = function (e) {
+					addProductHandler ( true );
+				};
+				
+				request.onerror = function (e) {
+					console.log(e.value);
+					addProductHandler ( false );
+				};
+			}
+		}
+		
+		catch (e) {
+			console.log(e);
+		}
+		
+		var addProductHandler = function ( result ) {
+			console.log( result );
+		}
+		
+	}
+	
+	dbConnector.deleteProduct = function ( material ) {
+
+		if ( localDatabase != null && localDatabase.db != null ) {
+			
+			
+			var store = localDatabase.db.transaction("products").objectStore("products");
+			var request = store.openCursor();
+			var products = new Array();
+			var pointer = -1;
+			
+			var Product = function ( config ) {
+				this.material = config.material;
+				this.price = config.price;
+			}
+			
+			request.onsuccess = function( evt ) {
+				
+				var cursor = evt.target.result;
+				
+				if ( cursor ) {
+				
+					if ( cursor.value.matIndex ===  material ) {
+						
+						var deleteRequest = localDatabase.db.transaction( ["products"] , "readwrite" ).objectStore("products").delete( cursor.key );
+						deleteRequest.onsuccess = function( ev ) {
+							
+							console.log("deleted id:" + cursor.key + " !");
+							
+						}
+					
+					}
+					
+					cursor.continue(); 				
+				
+				}
+			}	
+		}		
+	}
+
+	dbConnector.fetchAll = function () {
+	
+		console.log('fetching');
+			
+		if ( localDatabase != null && localDatabase.db != null ) {
+		
+		console.log('localDatabase NOT null');
+			
+			try {
+				var store = localDatabase.db.transaction("products").objectStore("products");
+				
+				var request = store.openCursor();
+				var products = new Array();
+				var pointer = -1;
+				
+				var Product = function ( config ) {
+					this.material = config.material;
+					this.price = config.price;
+				}
+				
+				request.onsuccess = function( evt ) {
+					
+					var cursor = evt.target.result;
+					pointer++;
+					
+					if ( cursor ) {
+					
+						products[pointer] = new Product ({	
+							material : cursor.value.matIndex,
+							price : cursor.value.priceIndex
+						});
+						
+						cursor.continue(); 				
+					
+					} else {
+						onSuccessHandler ( products );
+					}
+				};
+				
+			}
+			catch (e) {
+				console.log('need to create db!');
+				dbConnector.createDatabase();
+			}
+		}
+		
+		var onSuccessHandler = function ( products ) {
+			App.Events.trigger( 'writeProducts', products );
+			console.log ( products );
+		}
+		
+	}	
+
+})();
+
+// dbConnector.createDatabase();
---
+++ main/materials.js
@@ -0,0 +1,186 @@
+var App = App || {};
+var dbConnector = dbConnector || {};
+
+(function () {
+	
+	// создаем пространство имен (NameSpace)
+	
+	App = {
+	
+		Models: {},
+		Views: {},
+		Collections: {},
+		Events : _.extend( {}, Backbone.Events )
+	
+	};
+	
+	App.Models.Material = Backbone.Model.extend({ 
+
+		material: 'empty',
+		price: 0
+	
+	});
+	
+	App.Views.Material = Backbone.View.extend({ // это вид модели
+		tagName: 'li',
+		initialize: function () {
+			//this.model.on('change:material', this.render, this);
+			//this.model.on('change:price', this.render, this);
+			this.model.on( 'destroy', this.remove, this );
+		},
+		events: {
+			'click .delete' : 'confirmRemove'
+		},
+		template: _.template( $('#material-price').html() ),
+		render: function () {
+			var strTemplate = this.template( this.model.toJSON() );
+			this.$el.html( strTemplate );
+		},
+		confirmRemove: function () {
+			if ( confirm('Вы действительно хотите удалить данную запись?') ) {
+				App.Events.trigger( 'destroyModel', this.model );  
+			}	
+		},
+		remove: function () {
+		
+			this.$el.remove();
+		
+		}
+	});
+	
+	App.Collections.List = Backbone.Collection.extend({
+		model: App.Models.Material,
+		initialize: function () {
+			
+			App.Events.on( 'destroyModel', this.destroyModel, this );
+			App.Events.on( 'addModel', this.addModel, this );
+			App.Events.on( 'fetchProducts', this.fetchProducts, this );
+			App.Events.on( 'writeProducts', this.writeProducts, this );
+			
+			dbConnector.openDatabase();
+			
+		},
+		fetchProducts: function () {
+			
+			dbConnector.fetchAll();
+			
+		},
+		writeProducts: function ( products ) {
+		
+			for ( var i=0; i<=products.length-1; i++ )
+			{   
+			
+				var strMaterial = new App.Models.Material({ 
+					material: products[i].material, 
+					price: products[i].price 
+				});
+				this.add ( strMaterial );
+				
+			}
+			
+		},
+		destroyModel: function ( model ) {
+			
+			dbConnector.deleteProduct( model.attributes.material );
+			model.destroy();
+			
+		},
+		addModel: function ( model ) {
+			this.add( model );
+			dbConnector.addProduct ( model.attributes.material, model.attributes.price );
+		}
+	});
+	
+	App.Views.List = Backbone.View.extend({  // это вид коллекции
+	
+		tagName: 'ul',
+		initialize: function () {
+			this.collection.on('add', this.addOne, this);
+		},
+		render: function () {
+			this.collection.each(this.addOne, this);
+			return this;
+		},
+		addOne: function(modelMaterial) {
+			var MaterialView = new App.Views.Material({ model: modelMaterial });
+			MaterialView.render();
+			this.$el.append( MaterialView.el );
+		}
+	
+	});
+	
+	App.Views.AddMaterial = Backbone.View.extend({
+		el: '#addMaterial',
+		tagName: 'addMaterial',
+		events: {
+			'keypress input': 'inputKeypress',
+		},
+		inputKeypress: function(e) {
+			if (e.which === 13) {
+				this.validateItem();
+			}
+		},
+		validateItem: function () {
+		
+			var strMaterial = $('#material').val().trim(); 	
+			var strPrice = $('#price').val().trim();
+			
+			if ( strMaterial === "" ) {
+				alert ( 'Пожалуйста, введите имя материала!' );
+				$('#material').val('');
+				$('#material').focus();
+				return false;
+			}
+			
+			for ( var i = 0; i < this.collection.length; i++ ) {
+				
+				if ( strMaterial === this.collection.models[i].attributes.material ) {
+					
+					alert ( 'Материал ' + strMaterial + ' уже существует! Повторений НЕ должно быть!' );
+					$('#material').focus();
+					return false;
+					
+				}
+				
+			}
+			
+			if ( isNaN( strPrice ) || strPrice < 0 )  {
+			
+				alert ( 'Цена указана неверно!' );
+				$('#price').focus();
+				return false;
+			
+			}
+			
+			this.addItem ( strMaterial, strPrice );
+		
+		},
+		addItem: function( strMaterial, strPrice ) {
+			
+			var modelMaterial = new App.Models.Material({ 
+				material: strMaterial, 
+				price: strPrice 
+			});
+			
+			App.Events.trigger( 'addModel', modelMaterial );
+			
+			this.clearTextBoxes();
+		},
+		clearTextBoxes: function() {
+			$('#material').val('');
+			$('#price').val('');
+			$('#material').focus();
+		}
+	});	
+	
+	var Materials = new App.Collections.List;
+	var addMaterial = new App.Views.AddMaterial( { collection: Materials } );
+	var viewMaterials = new App.Views.List( { collection: Materials } );
+	viewMaterials.render();
+	$('#table_holder').html( viewMaterials.el );
+	
+}()); 
+
+
+
+
