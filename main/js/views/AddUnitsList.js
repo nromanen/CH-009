0ee0aaabedfda0de8c870c108ec78dd0cd92e10c
Dima <Arxaos@ukr.net>
2013-05-15 21:19:17 +0300

    fixed bugs

--- main/js/views/AddUnitsList.js
+++ main/js/views/AddUnitsList.js
@@ -55,12 +55,7 @@ var App = App || {};
 					this.collection.add (newModel);
 					this.options.something.set("goodsPrice", this.options.something.get("goodsPrice")+newModel.get('goodsItemPrice') );
 					this.options.something.set("goodsCollection", this.collection);
-
-
-				this.collection.add (new App.Models.GoodsItem({units:this.model.get ( 'name' ), count:quantity}));
-				console.log(this.collection);
-				//this.options.something.set("goodsCollection", this.collection);
-				App.dbConnector.EditGoodsItems(this.options.something);
+ 					App.dbConnector.EditGoodsItems(this.options.something);
 				
 				
 				}
