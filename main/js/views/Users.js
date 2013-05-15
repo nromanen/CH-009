d828ded07df2a8c5b607bd72b77391449240ffb5
November11 <mshematc@chclass15.training.local>
2013-05-15 18:26:03 +0300

    delete buttons from customer and accountant

--- main/js/views/Users.js
+++ main/js/views/Users.js
@@ -166,6 +166,13 @@ var App = App || {};
 
 			viewProducts.render();
 			$('.delete_goods').remove();
+			$('.delete').remove();
+			$('.edit_right').remove();
+			$('#products table tr th:nth-child(3)').hide();
+			$('#products table tbody tr td:nth-child(3)').hide();
+			$('#products table tr th:nth-child(4)').hide();
+			$('#products table tbody tr td:nth-child(4)').hide();
+			$('.colspan4').attr('colspan', '2');
 			$('.buttonPlace').html("")
 			$('.goods_info').html("");
 
@@ -185,13 +192,18 @@ var App = App || {};
 
 			viewProducts.render();
 			$('.buttonPlace').html("")
-			// rendering the content of the Units Tab
+			// rendering the content of the Goods Tab
 			var viewUnits = new App.Views.UnitsList( { collection: App.Units } );
 			viewUnits.render();
 			$('.delete').remove();
 			$('.edit_right').remove();
 			$('.delete_goods').remove();
 			$('.buttonPlace').html("")
+			$('#products table tr th:nth-child(3)').hide();
+			$('#products table tbody tr td:nth-child(3)').hide();
+			$('#products table tr th:nth-child(4)').hide();
+			$('#products table tbody tr td:nth-child(4)').hide();
+			$('.colspan4').attr('colspan', '2');
 			
 			// rendering the content of the Units Tab
 			var viewUnits = new App.Views.UnitsList( { collection: App.Units } );
@@ -201,6 +213,16 @@ var App = App || {};
 				active  : '',
 			}) );
 			$('#units').append( viewUnits.el );
+			$('.edit_right').remove();
+			$('.delete_unit').remove();
+			$('.edit').remove();
+			$('.delete').remove();
+			$('#units table tr th:nth-child(3)').hide();
+			$('#units table tbody tr td:nth-child(3)').hide();
+			$('#units table tr th:nth-child(4)').hide();
+			$('#units table tbody tr td:nth-child(4)').hide();
+			$('td a').remove();
+			$('.colspan4').attr('colspan', '2');
 
 			// rendering the content of the Materials Tab
 			var viewMaterials = new App.Views.List( { collection: App.Materials } );
