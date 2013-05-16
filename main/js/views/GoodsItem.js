1313f4e4fd0d6ef63c5ab858c516b55fcf9ed7be
unknown <dkozatc@chclass13.training.local>
2013-05-16 19:25:56 +0300

    make editing

--- main/js/views/GoodsItem.js
+++ main/js/views/GoodsItem.js
@@ -11,9 +11,10 @@ var App = App || {};
 		},
 		events: {
 			'click .delete' : 'confirmRemove',
-			'click .editCount' : 'changeCount',
-			'keypress .editUnitsCount': 'updateOnEnter',
-			'blur .editUnitsCount': 'close'
+			'click .edit' : 'changeCount',
+			'keypress .edit_units_count': 'updateOnEnter',
+			'blur .edit_units_count': 'close'
+			
 		},
 		render: function () {	
 			
@@ -27,7 +28,7 @@ var App = App || {};
 			}
 
 			this.$el.html( strTemplate );
-			this.$input = this.$('.editUnitsCount');
+			this.$input = this.$('.edit_units_count');
 			this.$input.val( this.model.get( 'count' ) );
 
 		},
@@ -42,21 +43,24 @@ var App = App || {};
 			this.$el.remove();
 		},
 		changeCount: function () {
-			this.$el.addClass('editingCount');
+			this.$el.addClass('editing');
 			this.$input.focus();
 		},
 		close: function () {
 			var value = this.$input.val().trim();
+
 			 if ( isNaN ( value )  || value <0 || value == '') {
-				this.$el.removeClass('editingCount');
+				this.$el.removeClass('editing');
+				alert(value);
 				this.render();
+			
 				return;
 			}	
 			this.options.goodsModel.set('goodsPrice', this.options.goodsModel.get('goodsPrice')-this.model.get('goodsItemPrice'));
-			App.Events.trigger('newUnitsCount', this.model, value, newPrice);
+			App.Events.trigger('newUnitsCount', this.model, value);
 			this.options.goodsModel.set('goodsPrice', this.options.goodsModel.get('goodsPrice')+this.model.get('goodsItemPrice'));
 			App.dbConnector.EditGoodsItems( this.options.goodsModel );
-			this.$el.removeClass('editingCount');
+			this.$el.removeClass('editing');
 			
 		},
 		updateOnEnter: function (e) {
